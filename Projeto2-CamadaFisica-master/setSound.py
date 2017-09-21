44276bc250e599812e276376dc3aaf0ecb1b8e47
Daniel Ruhman <daniruhman@gmail.com>
2017-09-21 09:44:42 -0300

    generate sound function

--- Projeto2-CamadaFisica-master/setSound.py
+++ Projeto2-CamadaFisica-master/setSound.py
@@ -2,26 +2,29 @@ import sounddevice as sd
 import matplotlib.pyplot as plt
 import numpy as np
 
-f1=666
-f2=69420
-
 fs = 44100
-t=3
-om=(2 * np.pi *f1)
-om1=(2 * np.pi *f2)
+t=1
 
-a = np.sin(tempo*om)
-b = np.sin(tempo*om1)
+def som(list,num):
+    f1 = list[0]
+    f2 = list[1]
 
-y=a+b
 
-time=np.linspace(0, t, fs*t)
-print (len(time), len(y))
-plt.plot(time, y)
-plt.xlim(0,0.015)
-plt.xlabel('tempo')
-plt.ylabel('onda')
-plt.show()
+    om=(2 * np.pi *f1)
+    om1=(2 * np.pi *f2)
 
-sd.play(y, fs)
-sd.wait()
+    a = np.sin(tempo*om)
+    b = np.sin(tempo*om1)
+
+    y=a+b
+
+    time=np.linspace(0, t, fs*t)
+    plt.close("all")
+    plt.plot(time, y)
+    plt.xlim(0,0.015)
+    plt.xlabel('tempo')
+    plt.ylabel('onda')
+    plt.show()
+
+    sd.play(y, fs)
+    sd.wait()
